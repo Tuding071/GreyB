685f4ce03451fdf34a3046f7348a4bf8c4047d19
Tuding071 <davidpenaranda071@gmail.com>
2026-06-11 12:57:43 +0800

    Update Keyboard.kt

--- Keyboard.kt
+++ Keyboard.kt
@@ -1,7 +1,5 @@
 package com.greyb.keyboard
 
-import android.inputmethodservice.InputMethodService
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -20,75 +18,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.*
-import androidx.lifecycle.setViewTreeLifecycleOwner
-import androidx.lifecycle.setViewTreeViewModelStoreOwner
-import androidx.savedstate.SavedStateRegistry
-import androidx.savedstate.SavedStateRegistryController
-import androidx.savedstate.SavedStateRegistryOwner
-import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 
-class GreyBIME : InputMethodService(),
-    LifecycleOwner,
-    SavedStateRegistryOwner,
-    ViewModelStoreOwner {
-
-    private val lifecycleRegistry = LifecycleRegistry(this)
-    private val savedStateRegistryController = SavedStateRegistryController.create(this)
-    private val store = ViewModelStore()
-
-    override val lifecycle: Lifecycle get() = lifecycleRegistry
-    override val savedStateRegistry: SavedStateRegistry get() = savedStateRegistryController.savedStateRegistry
-    override val viewModelStore: ViewModelStore get() = store
-
-    private lateinit var composeView: ComposeView
-
-    override fun onCreate() {
-        super.onCreate()
-        savedStateRegistryController.performRestore(null)
-        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    }
-
-    override fun onCreateInputView(): View {
-        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
-        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
-
-        composeView = ComposeView(this).apply {
-            setViewTreeLifecycleOwner(this@GreyBIME)
-            setViewTreeSavedStateRegistryOwner(this@GreyBIME)
-            setViewTreeViewModelStoreOwner(this@GreyBIME)
-            setContent {
-                GreyBTheme {
-                    GreyBKeyboard(onKeyPress = { key -> handleKeyPress(key) })
-                }
-            }
-        }
-        return composeView
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        store.clear()
-        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    }
-
-    override fun onEvaluateFullscreenMode(): Boolean = false
-
-    override fun onComputeInsets(outInsets: InputMethodService.Insets?) {
-        super.onComputeInsets(outInsets)
-        if (outInsets != null) {
-            outInsets.contentTopInsets = outInsets.visibleTopInsets
-        }
-    }
+class GreyBIME : LifecycleOwnerIME() {
 
     override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
         super.onStartInputView(info, restarting)
-        composeView.requestLayout()
+    }
+
+    @Composable
+    override fun KeyboardContent() {
+        GreyBTheme {
+            GreyBKeyboard(onKeyPress = { key -> handleKeyPress(key) })
+        }
     }
 
     private fun handleKeyPress(key: KeyDef) {
